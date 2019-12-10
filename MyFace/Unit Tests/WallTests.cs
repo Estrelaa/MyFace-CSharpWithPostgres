fe9f4858bfba788ff96fb7cdb169462284bf8385
Kieran Winfield <K.Winfield@ucas.ac.uk>
2019-12-10 10:02:21 +0000

    Added a unit test to make that the Posts Database exist

---
+++ MyFace/Unit Tests/WallTests.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using MyFace.DataAccess;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFace.Unit_Tests
+{
+    [TestFixture]
+    public class WallTests
+    {
+        [Test]
+        public void CreateAPostInTheDatabase()
+        {
+            using (var db = ConnectionHelper.CreateSqlConnection())
+            {
+                Assert.IsNotNull(db.Query<string>("SELECT * FROM \"Posts\""));
+            }
+        }
+    }
+}
